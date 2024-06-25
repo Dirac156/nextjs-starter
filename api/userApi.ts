3b7ff3aa7fdf0dcee612d668b6451bb2519cba72
dirac156 <dmurairimukongya@gmail.com>
2024-06-25 17:45:42 +0200

    added oauth: google and tiktok

--- api/userApi.ts
+++ api/userApi.ts
@@ -1,10 +1,20 @@
 import { AxiosError, User } from "@/global";
+import { v4 } from "uuid";
 
 class UserAPI {
   async signInUser(user: User): Promise<User> {
     try {
       // TODO: make api call
-      return { email: "info@gmail.com", firstName: "", lastName: "" };
+      return { email: "info@gmail.com", firstName: "", lastName: "", id: v4() };
+    } catch (error: any) {
+      throw new Error("error", error?.message);
+    }
+  }
+
+  async signUpUser(user: User): Promise<User> {
+    try {
+      // TODO: make api call
+      return { email: "info@gmail.com", firstName: "", lastName: "", id: v4() };
     } catch (error: any) {
       throw new Error("error", error?.message);
     }
--- app/(auth)/sso-fallback-signin/page.tsx
+++ app/(auth)/sso-fallback-signin/page.tsx
@@ -1,14 +1,63 @@
 "use client";
 
+import Spinner from "@/components/spinner/spinner";
+import { useToast } from "@/components/ui/use-toast";
+import { useSignIn } from "@/hooks";
+import { useUserContext } from "@/providers";
 import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function SignInCallbackComplete() {
+  const router = useRouter();
+  const { toast } = useToast();
   const { user } = useUser();
+  const {
+    data: signInUser,
+    mutate: signIn,
+    isSuccess: isSignIn,
+    isError: isSignInError,
+  } = useSignIn();
+  const { signIn: signUser } = useUserContext();
+
+  useEffect(() => {
+    if (isSignInError) {
+      toast({
+        variant: "destructive",
+        description: "Authentication Failed!",
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSignInError]);
+
+  useEffect(() => {
+    router.prefetch("/store");
+    router.prefetch("/");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (isSignIn) {
+      signUser(signInUser);
+      router.push("/store");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSignIn]);
+
   useEffect(() => {
     if (user) {
-      // TODO: @Dirac save users
+      signIn({
+        email: user.emailAddresses[0].emailAddress as string,
+        id: user.id,
+        firstName: user.firstName as string,
+        lastName: user.lastName as string,
+      });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user]);
-  return <div></div>;
+  return (
+    <div>
+      <Spinner />
+    </div>
+  );
 }
